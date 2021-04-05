d883b13117559d824c4fe21e0e3ddd47c010dbad
Christian-B-ASU <78243321+Christian-B-ASU@users.noreply.github.com>
2021-04-04 22:09:51 -0500

    Uploaded source files

---
+++ CurrentWeatherCodeCUSIApp/CurrentWeatherCodeCUSI/Models/CurrentWeatherModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrentWeatherCodeCUSI.Models
+{
+    public class CurrentWeatherModel
+    {
+        public Weather[] weather { get; set; }
+        public Main Main { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+}
---
+++ CurrentWeatherCodeCUSIApp/CurrentWeatherCodeCUSI/Models/Main.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrentWeatherCodeCUSI.Models
+{
+    public class Main
+    {
+        public float temp { get; set; }
+        public float feels_like { get; set; }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+        public int humidity { get; set; }
+    }
+}
